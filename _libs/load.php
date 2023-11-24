9ffe82ef5fa5fd0debe1073174736a6176aee123
tholkappiar <tholkappiar2003@gmail.com>
2023-11-24 06:38:27 +0000

    fix: updated file names - work against dir buster.

--- _libs/load.php
+++ _libs/load.php
@@ -2,7 +2,7 @@
 
 function load_template($template_name){
     // print_r($template_name);
-    include __DIR__.("/../_templates/$template_name.php");
+    include $_SERVER['DOCUMENT_ROOT'].("/SnapEase/_templates/_$template_name.php");
 }
 
 ?>
