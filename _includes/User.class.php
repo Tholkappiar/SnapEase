4988944bfcfdaf2ab95a4ee43bf551ed98b9e196
tholkappiar <tholkappiar2003@gmail.com>
2023-12-02 08:39:10 +0000

    Used Magic functions to reduce boiler plate code.

--- _includes/User.class.php
+++ _includes/User.class.php
@@ -2,8 +2,8 @@
 
 class User
 {
+    // $conn -> holds the connection , $id -> holds the id of the user .
     private $conn;
-    private $first_name;
     private $id;
 
     public static function signup($first_name, $last_name, $email_addr, $pass)
@@ -27,9 +27,6 @@ class User
 
     public static function login($email, $password)
     {
-
-        // md5 hash for the password
-        // $password = md5($password);
         // query to be passed
         $query = "SELECT * FROM `_auth` WHERE `email` = '$email'";
         // database connection
@@ -39,7 +36,6 @@ class User
         // checking the table for the email and password
         if ($result->num_rows == 1) {
             $row = $result->fetch_assoc();
-
             if (password_verify($password, $row["password"])) {
                 session_start();
                 return $row['password'];
@@ -51,78 +47,132 @@ class User
         }
     }
 
-    public function __construct($first_name)
+    public function __construct($username)
     {
-
         if(!$this->conn){
             $this->conn = Database::getConnection();
         }
-        $query = "SELECT `id` FROM `_auth` WHERE `first_name`= '$first_name' LIMIT 1;";
-
+        $query = "SELECT `id` FROM `_auth` WHERE `username`= '$username' LIMIT 1;";
         $result = $this->conn->query($query);
         if ($result->num_rows == 1) {
             $this->id = $result->fetch_assoc()["id"];
-            print_r($this->id);
+            // print_r($this->id);
         } else {
-            print_r("user not found");
-        }
-    }
-    private function get_details_by_column($column)
-    {
-        if(!$this->conn){
-            $this->conn = Database::getConnection();
-        }
-        $query = "SELECT `$column` FROM `_users` WHERE `id` = $this->id";
-        $result = $this->conn->query($query);
-        if($result->num_rows) {
-            return $result->fetch_assoc()["$column"];
-        } else {
-            return "somethig went wrong in the column !";
+            throw new Exception("user doesn't found");
         }
     }
 
-    private function save_db_details($column, $data)
-    {
-        if (!$this->conn) {
-            $this->conn = Database::getConnection();
+    // Using __call magic function to reduce the boiler plate codes -> getters and setters
+    public function __call($name, $attributes){
+        print("this is _call \n");
+        // To get the property name without the get or set in the start
+        $property = preg_replace("/[^0-9a-zA-Z]/", "", substr($name, 3));
+        // To convert camelCase to Snake_Case and meke it lowerCase
+        $property = strtolower(preg_replace('/\B([A-Z])/', '_$1', $property));
+
+        if(substr($name, 0, 3) == "set"){
+            return $this->_set_data($property, $attributes[0]);    
+        } else if(substr($name, 0, 3) == "get"){
+            return $this->_get_data($property);
+        } else {
+            throw new Exception("User::__call function : $name");
         }
-        $sql = "UPDATE `_users` SET `$column`='$data' WHERE `id`='$this->id';";
-        if ($this->conn->query($sql)) {
-            return "data saved to DB !!";
+    }
+
+    public function _set_data($name, $value){
+        if(!$this->conn){
+            $conn = Database::getConnection();
+        };
+        print_r("name : $name value : $value");
+        $query = "UPDATE `_users` SET `$name`='$value' WHERE `id`=$this->id;";
+        if($this->conn->query($query)){
+            return true;
         } else {
             return $this->conn->error;
         }
     }
 
-    public function setBio($bio)
-    {
-        return $this->save_db_details("bio","$bio");
+    public function _get_data($name){
+        if(!$this->conn){
+            $conn = Database::getConnection();
+        }
+        $query = "SELECT $name FROM `_users` WHERE `id`='$this->id';";
+        $result = $this->conn->query($query);
+        if($result->num_rows){
+            $row = $result->fetch_assoc();
+            return $row[$name];
+        } else {
+            return $result->error;
+        }
     }
 
-    public function getBio()
-    {
-        return $this->get_details_by_column("bio");
-    }
 
-    public function setAvatar($avatar)
-    {
-        return $this->save_db_details("avatar","$avatar");
-    }
 
-    public function getAvatar()
-    {
-        return $this->get_details_by_column("avatar");
-    }
-    public function setDOB($DOB){
-        return $this->save_db_details("dob","$DOB");
-    }
-    public function getDOB(){
-        return $this->get_details_by_column("dob");
-    }
-    public function getInsta(){
-        return $this->get_details_by_column("instagram");
-    }
-    public function setInsta($insta){
-        return $this->save_db_details("instagram","$insta");
-    }
+
+
+
+
+    // private function get_details_by_column($column)
+    // {
+    //     if(!$this->conn){
+    //         $this->conn = Database::getConnection();
+    //     }
+    //     $query = "SELECT `$column` FROM `_users` WHERE `id` = $this->id";
+    //     $result = $this->conn->query($query);
+    //     if($result->num_rows) {
+    //         return $result->fetch_assoc()["$column"];
+    //     } else {
+    //         return "somethig went wrong in the column !";
+    //     }
+    // }
+
+    // private function save_db_details($column, $data)
+    // {
+    //     if (!$this->conn) {
+    //         $this->conn = Database::getConnection();
+    //     }
+    //     $sql = "UPDATE `_users` SET `$column`='$data' WHERE `id`='$this->id';";
+    //     if ($this->conn->query($sql)) {
+    //         return "data saved to DB !!";
+    //     } else {
+    //         return $this->conn->error;
+    //     }
+    // }
+
+
+
+
+
+    // Getters and setters 
+    // public function setBio($bio)
+    // {
+    //     return $this->save_db_details("bio","$bio");
+    // }
+
+    // public function getBio()
+    // {
+    //     return $this->get_details_by_column("bio");
+    // }
+
+    // public function setAvatar($avatar)
+    // {
+    //     return $this->save_db_details("avatar","$avatar");
+    // }
+
+    // public function getAvatar()
+    // {
+    //     return $this->get_details_by_column("avatar");
+    // }
+    // public function setDOB($DOB){
+    //     return $this->save_db_details("dob","$DOB");
+    // }
+    // public function getDOB(){
+    //     return $this->get_details_by_column("dob");
+    // }
+    // public function getInsta(){
+    //     return $this->get_details_by_column("instagram");
+    // }
+    // public function setInsta($insta){
+    //     return $this->save_db_details("instagram","$insta");
+    // }
 }
--- test/test.php
+++ test/test.php
@@ -1,93 +1,9 @@
-
-    <pre>
-    <?
-
-    // hashes
-
-    $str = isset($_GET['key']) ? ($_GET['key']) : 'thols';
-    $hash = md5($str);
-
-    print($hash);
+<?php
 
 
+include_once("../_libs/load.php");
 
-
-
-
-
-
-
-
-    // ------------------- sessions --------------------
-
-    // class sessions{
-
-    //     public static function ses_start(){
-    //         session_start();
-    //     }
-
-    //     public static function ses_status(){
-    //         return session_status();
-    //     }
-
-    //     public static function ses_destory(){
-    //         session_destroy();
-    //     }
-
-    //     public static function ses_encode(){
-    //         return session_encode();
-    //     }
-
-    //     public static function ses_decode($data){
-    //         return session_decode($data);
-    //     }
-
-    // }
-
-    // sessions::ses_start();
-
-    // $enc =  sessions::ses_encode();
-
-    // print_r($_SESSION['time']);
-    // if(isset($_SESSION['time'])){
-    //     print("<br> time already exist !..... $_SESSION[time]");
-    // }else{
-    //     $_SESSION['time'] = time();
-    //     print("<br> printing new value -> $_SESSION[time]");
-    // }
-
-    // print "<br> this is the encoded string : $enc";
-
-    // $dec = sessions::ses_decode($enc);
-    // print "<br> this is the decoded string : $dec";
-
-    // print("<br> this is the status : " . sessions::ses_status());
-
-    //------------------ super globals ------------------
-
-    // print 'super gobal variables <br>';
-    // print_r($_SERVER['DOCUMENT_ROOT']."<br>");
-
-    // echo "GET method <br>";
-    // print_r($_GET);
-
-    // echo "POST method <br>";
-    // print_r($_POST);
-
-    // echo "COOKIE method <br>";
-    // print_r($_COOKIE);
-
-    // echo "FILES method <br>";
-    // print_r($_FILES);
-
-    // echo "GLOBALS method <br>";
-    // // print_r($_GLOBALS);
-
-    // echo "temp dir <br>";
-    // echo sys_get_temp_dir();
-
+$obj1 = new User("test1");
+$obj1->setInstagram("insta new");
     
-
 ?>
-    </pre>
-   
