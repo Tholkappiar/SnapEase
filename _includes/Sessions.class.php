9f2061e3b6072a6d78dc1578b33e6595a8b1fc4b
tholkappiar <tholkappiar2003@gmail.com>
2023-12-22 18:08:04 +0530

    refactor: Seperate functions for each operation.

--- _includes/Sessions.class.php
+++ _includes/Sessions.class.php
@@ -33,30 +33,18 @@ class Sessions
             $user_agent = $this->getUserAgent();
             $login_time = date("Y-m-d H:i:s", substr($this->getLoginTime(), 0, 10));
     
-            // Creating the token using ip and user agent
+            // Creating the token using FingerPrint js.
             $token = $fingerprint;
             
-            // Check if the session is already in the session table
-            $check_sql_uid = "SELECT `token` FROM `_session` WHERE `uid`='$this->uid';";
-            $check_result = $this->conn->query($check_sql_uid);
-    
-            if ($check_result->num_rows > 0 and $this->isActive()) {
-
-                // Update the existing record
-                $update_sql = "UPDATE `_session` SET `token`='$token', `login_time`='$login_time', `ip`='$ip', `user_agent`='$user_agent' WHERE `uid`='$this->uid';";
-                if ($this->conn->query($update_sql)) {
+            if ( $this->isSessionExists() and $this->isActive()) {
+                if ($this->updateSession($token,$login_time,$ip,$user_agent)) {
                     $_SESSION['token'] = $token;
-                    // $this->deactivate();
                     return true;
                 } else {
                     throw new Exception("Sessions.class.php :: Update Error " . mysqli_error($this->conn));
                 }
             }  else {
-                // Inserting a new record into the table
-                $session_sql = "INSERT INTO `_session` (`uid`, `token`, `login_time`, `ip`, `user_agent`)
-                                VALUES ('$this->uid', '$token', '$login_time', '$ip', '$user_agent');";
-    
-                if ($this->conn->query($session_sql)) {
+                if ($this->insertSession($token, $login_time, $ip, $user_agent)) {
                     $_SESSION['token'] = $token;
                     return true;
                 } else {
@@ -77,10 +65,8 @@ class Sessions
         if(!$this->conn) {
             $this->conn = Database::getConnection();
         }
-        // get the token from the DB and compare with session token value
-        $auth_sql = "SELECT `token` FROM `_session` WHERE `uid`='$this->uid';";
-        $token_hash = $this->conn->query($auth_sql)->fetch_assoc()["token"];
-        
+
+        $token_hash = $this->getToken();
         if($_SESSION['token'] == $token_hash) {
             return true;
         } else {
@@ -107,15 +93,6 @@ class Sessions
         return $_SERVER["HTTP_USER_AGENT"];
     }
 
-    // To Deactivate the Account. Needs the uid.
-    public function deactivate()
-    {
-        $deactivate_sql = "UPDATE `_session` SET `active`='0' WHERE `uid`='$this->uid';";
-        if ($this->conn->query($deactivate_sql)) {
-            print("The Account is Deactivated !");
-        }
-    }
-
     /**
      * Returns the Login time of the user.
      * @return string
@@ -124,7 +101,14 @@ class Sessions
         return $_SERVER['REQUEST_TIME'];
     }
 
-    /**
+    // Check if the session is already in the session table
+    public function isSessionExists(){
+        $check_sql_uid = "SELECT `token` FROM `_session` WHERE `uid`='$this->uid';";
+        $check_result = $this->conn->query($check_sql_uid);
+        return $check_result->num_rows > 0;
+    }
+
+     /**
      * Returns the boolean based on the Account Active Status.
      * @return boolean
      */
@@ -134,4 +118,46 @@ class Sessions
         return $active_result['active']==1?true:false;
     }
 
+     // Update the existing record
+     public function updateSession($token, $login_time, $ip, $user_agent){
+        $update_sql = "UPDATE `_session` SET `token`='$token', `login_time`='$login_time', 
+                      `ip`='$ip', `user_agent`='$user_agent' WHERE `uid`='$this->uid';";
+
+        if($this->conn->query($update_sql)){
+            return true;
+        } else {
+            throw new Exception("Sessions.class.php :: in updateSession " . mysqli_error($this->conn));
+        }
+    }
+
+    // Inserting a new record into the table
+    public function insertSession($token, $login_time, $ip, $user_agent){
+        $session_sql = "INSERT INTO `_session` (`uid`, `token`, `login_time`, `ip`, `user_agent`)
+                        VALUES ('$this->uid', '$token', '$login_time', '$ip', '$user_agent');";
+        if ($this->conn->query($session_sql)) {
+            return true;
+        } else {
+            throw new Exception("Sessions.class.php :: in insertSession " . mysqli_error($this->conn));
+        }
+    }
+
+    // To get the token for the user.
+    public function getToken(){
+        $auth_sql = "SELECT `token` FROM `_session` WHERE `uid`='$this->uid';";
+        $result = $this->conn->query($auth_sql)->fetch_assoc()["token"];
+        if($result) {
+            return $result;
+        } else {
+            throw new Exception("Sessions.class.php :: in getToken " . mysqli_error($this->conn));
+        }
+    }
+
+    // To Deactivate the Account. Needs the uid.
+    public function deactivate()
+    {
+        $deactivate_sql = "UPDATE `_session` SET `active`='0' WHERE `uid`='$this->uid';";
+        if ($this->conn->query($deactivate_sql)) {
+            print("The Account is Deactivated !");
+        }
+    }  
 }
