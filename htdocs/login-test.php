a4dc2e0435a65e59f7552882b9c84dfe66dc5804
tholkappiar <tholkappiar2003@gmail.com>
2023-12-25 23:51:51 +0530

    feat: new login page.

---
+++ htdocs/login-test.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>login-test</title>
+    <link rel="stylesheet" href="login-test.css" />
+</head>
+
+<body>
+    this is ncoe
+    <div class="login-root">
+            <div class="container-root" >
+            <div id=container>
+                Make
+                <div id=flip>
+                    <div>
+                        <div>wOrK</div>
+                    </div>
+                    <div>
+                        <div>lifeStyle</div>
+                    </div>
+                    <div>
+                        <div>Everything</div>
+                    </div>
+                </div>
+                AweSoMe!
+            </div>
+                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Vero cumque, molestiae nesciunt alias voluptate est cupiditate molestias minima harum, illum eius saepe sed ratione nobis?
+            </div>
+            
+
+        <div class="image"> image</div>
+        <div class="login"> login</div>
+    </div>
+
+
+</body>
+
+</html>
