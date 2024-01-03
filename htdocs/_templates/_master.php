46d30bf61f130be5ef042fcd93f651ac8af7384e
tholkappiar <tholkappiar2003@gmail.com>
2024-01-03 18:14:21 +0530

    fix: masonry race condition using imagesLoaded.

--- htdocs/_templates/_master.php
+++ htdocs/_templates/_master.php
@@ -1,9 +1,24 @@
 <?php
 
-    include_once "_libs/load.php";
+include_once "_libs/load.php";
 
-    Sessions::load_template("_header");
-    Sessions::current_script();
-    Sessions::load_template("_footer");
+Sessions::load_template("_header");
+Sessions::current_script();
+Sessions::load_template("_footer");
 
+?>
 
+<script src="https://cdn.jsdelivr.net/npm/masonry-layout@4.2.2/dist/masonry.pkgd.min.js" integrity="sha384-GNFwBvfVxBkLMJpYMOABq3c+d3KnQxudP/mGPkzpZSTYykLBNsZEnG2D9G/X/+7D" crossorigin="anonymous"></script>
+<script src="https://unpkg.com/imagesloaded@5/imagesloaded.pkgd.min.js"></script>
+<script>
+    // init Masonry
+    var $grid = $('.row').masonry({
+        // itemSelector: '.col',
+        // columnWidth: '.col',
+        percentPosition: true
+    });
+    // layout Masonry after each image loads
+    $grid.imagesLoaded().progress(function() {
+        $grid.masonry('layout');
+    });
+</script>
