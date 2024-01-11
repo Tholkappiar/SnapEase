668d19e8cc33feb16f63f7cafafce4ca6aa5bc74
tholkappiar <tholkappiar2003@gmail.com>
2024-01-11 19:05:19 +0530

    feat: toast message -js file

--- htdocs/_templates/_master.php
+++ htdocs/_templates/_master.php
@@ -13,6 +13,7 @@ Sessions::load_template("_footer");
 <script src="https://cdn.jsdelivr.net/npm/masonry-layout@4.2.2/dist/masonry.pkgd.min.js" integrity="sha384-GNFwBvfVxBkLMJpYMOABq3c+d3KnQxudP/mGPkzpZSTYykLBNsZEnG2D9G/X/+7D" crossorigin="anonymous"></script>
 <script src="/js/dialog.js"></script>
 <script src="/js/app.min.js"></script>
+<script src="/js/toast.js"></script>
 
  <!-- This the dummy division which is used to create the dialogs by clone it  -->
 <div id="modalsGarbage">
