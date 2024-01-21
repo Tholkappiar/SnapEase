07a4fc529ba05eb745e47b833e6a5f448e72d410
Tholkappiar <tholkappiar2003@gmail.com>
2024-01-21 16:57:32 +0000

    feat: profile modal in header.

--- htdocs/_templates/All_post.php
+++ htdocs/_templates/All_post.php
@@ -38,4 +38,5 @@
       <?php
       }
       ?>
+      
       <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet">
--- htdocs/_templates/_header.php
+++ htdocs/_templates/_header.php
@@ -3,6 +3,9 @@
 
 <head>
   <script src="assets/js/color-modes.js"></script>
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
 
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -13,7 +16,7 @@
 
   <link rel="canonical" href="https://getbootstrap.com/docs/5.3/examples/headers/">
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@docsearch/css@3">
-
+  <script src="https://code.jquery.com/jquery-3.7.1.min.js" integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo=" crossorigin="anonymous"></script>
 
 
 
@@ -27,21 +30,42 @@
       </a>
 
       <ul class="nav nav-pills">
-        <li class="nav-item"><a href="/All_post.php" class="nav-link active" aria-current="page"><i class="fa-solid fa-magnifying-glass"></i></a></li>
-        <? 
-          if(Sessions::isAuthenticated())
-          {
+        <li class="nav-item"><a href="/All_post.php" class="nav-link" aria-current="page"><i class="fa-solid fa-magnifying-glass"></i></a></li>
+        <?
+        if (Sessions::isAuthenticated()) {
         ?>
-            <li class="nav-item"><a href="/?logout" class="nav-link">Logout</a></li>
-            <li class="nav-item"><i class="fa-regular fa-user nav-link"></i></li>
-        <? 
-          }  else { 
-            ?>
-            <li class="nav-item"><a href="/login.php" class="nav-link">Login</a></li>
-            <?
-          }
+          <li class="nav-item"><a href="/?logout" class="nav-link">Logout</a></li>
+          <li id="profile" class="nav-item">
+            <a class="fa-regular fa-user nav-link" data-bs-toggle="modal" data-bs-target="#profileModal"></a>
+          </li>
+        <?
+        } else {
+        ?>
+          <li class="nav-item"><a href="/login.php" class="nav-link">Login</a></li>
+        <?
+        }
         ?>
       </ul>
+      <div class="modal fade" id="profileModal" tabindex="-1" aria-labelledby="profileModalLabel" aria-hidden="true">
+        <div class="modal-dialog modal-dialog-centered">
+          <div class="modal-content">
+            <div class="modal-header">
+              <h5 class="modal-title" id="exampleModalCenterTitle">Modal title</h5>
+              <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body">
+              <p>This is a vertically centered modal.</p>
+            </div>
+            <div class="modal-footer">
+              <!-- <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button> -->
+              <button type="button" class="btn btn-primary">Save changes</button>
+            </div>
+          </div>
+        </div>
+      </div>
+
+
+
     </header>
   </div>
 
@@ -50,4 +74,3 @@
   </body>
 
 </html>
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
